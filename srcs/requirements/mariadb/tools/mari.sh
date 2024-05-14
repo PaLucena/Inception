a01d3c32464e37ee39213c68a2970986add455b3
Pablo Lucena González <pablolucena01@gmail.com>
2024-05-14 22:37:51 +0200

    feat: finished MariaDB

---
+++ srcs/requirements/mariadb/tools/mari.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cat << EOF > config.sql
+CREATE DATABASE IF NOT EXISTS ${MYSQL_NAME};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_NAME} TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+chmod 777 config.sql
+mv config.sql /run/mysqld/config.sql
+chown -R mysql:root /run/mysqld
+mariadbd --init-file /run/mysqld/config.sql
